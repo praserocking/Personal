80993cd8b8a02ba9c7087af0b0221d4a5b9581a3
Shenbaga Prasanna <praserocking@gmail.com>
2013-10-14 12:55:13 +0530

    Update ABCPATH.cpp

--- ABCPATH.cpp
+++ ABCPATH.cpp
@@ -8,33 +8,33 @@ char graph[50][50];
 vector<int>ans;
 int cnt;
 void bfs(int i,int j){
-pair<int,int>tmp;
-cnt=1;
-queue<pair<int,int> >q;
-q.push(make_pair(i,j));
-while(!q.empty()){
-tmp=q.front();
-q.pop();
-for(int k=-1;k<=1;k++)
-for(int l=-1;l<=1;l++)
-if(graph[tmp.first+k][tmp.second+l]==(graph[tmp.first][tmp.second]+1)&&(k!=0&&l!=0)){
-cnt++;
-q.push(make_pair(tmp.first+k,tmp.second+l));
-}
-}
-ans.push_back(cnt);
+  pair<int,int>tmp;
+  cnt=1;
+  queue<pair<int,int> >q;
+  q.push(make_pair(i,j));
+  while(!q.empty()){
+    tmp=q.front();
+    q.pop();
+    for(int k=-1;k<=1;k++)
+      for(int l=-1;l<=1;l++)
+        if(graph[tmp.first+k][tmp.second+l]==(graph[tmp.first][tmp.second]+1)&&(k!=0&&l!=0)){
+        cnt++;
+        q.push(make_pair(tmp.first+k,tmp.second+l));
+        }
+  }
+  ans.push_back(cnt);
 }
 int main(){
-int r,c;
-while(true){
-cin>>r>>c;
-if(r==0&&c==0)break;
-for(int i=0;i<r;i++)
-cin>>graph[i];
-for(int i=0;i<r;i++)
-for(int j=0;j<c;j++)
-if(graph[i][j]=='A')bfs(i,j);
-cout<<*max_element(ans.begin(),ans.end());
+  int r,c;
+  while(true){
+    cin>>r>>c;
+    if(r==0&&c==0)break;
+    for(int i=0;i<r;i++)
+      cin>>graph[i];
+    for(int i=0;i<r;i++)
+      for(int j=0;j<c;j++)
+        if(graph[i][j]=='A')bfs(i,j);
+    cout<<*max_element(ans.begin(),ans.end());
 }
-return 0;
+  return 0;
 }
